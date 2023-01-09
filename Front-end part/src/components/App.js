e217f0d8026062c6f6de03a3629e2be668f0d4f8
Eugene Zorin <ezorin5@yandex.ru>
2023-01-09 12:42:25 +0400

    frontend initial structure

--- Front-end part/src/components/App.js
+++ Front-end part/src/components/App.js
@@ -1,8 +1,18 @@
+import { Switch, Route } from 'react-router-dom';
 import '../assets/styles/App.css';
+import Portal from './Portal';
+import { MAIN_PAGE_ROUTE } from '../utils/constants';
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path={MAIN_PAGE_ROUTE} >
+        </Route>
+      </Switch>
+      <Portal>
+
+      </Portal>
     </div>
   );
 }
